3f01e1983d9c409a6a6df4e48ec9a58d3c62dd43
Dan <danohare99@googlemail.com>
2025-06-24 15:45:26 +0100

    added schema to main.py for 4 main functions

--- main.py
+++ main.py
@@ -10,7 +10,88 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Gets the file's contents and prints the max_chars of the content if over the max_chars limit.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file that has the content that needs to be obtained.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Execute the specified python file and returns relative output.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file that is required.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Creates and writes string to a file, if the file does not exist. If the file does exist, overwrites the file with the given string.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file that is required."),
+            "content": types.Schema(
+               type=types.Type.STRING,
+               description="The string that is to be the contents of the file"
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
+    ]
+)
+
 def main():
+   system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
    verbose = '--verbose' in sys.argv
    
    prompt_parts = [arg for arg in sys.argv[1:] if arg != '--verbose']
@@ -21,7 +102,12 @@ def main():
    ]
 
    response = client.models.generate_content(
-      model='gemini-2.0-flash-001', contents=messages
+      model='gemini-2.0-flash-001', 
+      contents=messages,
+      config=types.GenerateContentConfig(
+         tools=[available_functions],
+         system_instruction=system_prompt
+         )
    )
 
    if len(sys.argv) < 2:
@@ -33,7 +119,12 @@ def main():
       print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
       print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-   print(response.text)
+
+   if response.function_calls:
+      for call in response.function_calls:
+         print(f"Calling function: {call.name}({call.args})")
+   else:
+      print(response.text)
 
 if __name__ == "__main__":
     main()
